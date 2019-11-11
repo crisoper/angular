1c55bb5439c164fce8e719f8a47afc26f025001b
crisoper <crisoper@gmail.com>
2019-11-11 18:03:49 -0500

    Pasando datos componete padre a hija 9

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+//Importarmos la clase Input
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+	//Propiedad que llega como parámetro de la componente padre
+	@Input() valor: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  	this.foto = "https://i.pravatar.cc/250";
+  	this.likes = Math.trunc( Math.random()*1500 ) + 1;
+  }
+
+}
